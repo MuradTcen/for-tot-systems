e3a19c4676d115090636a05084b02b0722fa2847
MuradTcen <muradtcen@.gmail.com>
2020-06-07 18:50:34 +0500

    added fileStorageException

---
+++ src/main/java/moex/com/totsystems/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package moex.com.totsystems.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
