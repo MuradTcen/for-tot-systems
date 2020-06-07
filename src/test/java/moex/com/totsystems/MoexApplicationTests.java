152c65e3d540df664b4a40a4a9c0e96af42960c9
MuradTcen <muradtcen@.gmail.com>
2020-06-07 18:47:25 +0500

    added infrastructure files

---
+++ src/test/java/moex/com/totsystems/MoexApplicationTests.java
@@ -0,0 +1,13 @@
+package moex.com.totsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
