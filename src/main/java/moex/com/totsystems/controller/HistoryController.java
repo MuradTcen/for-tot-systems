d2f159c9727ce7c2e41d2f0c25b66901cc49efe8
MuradTcen <muradtcen@.gmail.com>
2020-06-10 00:16:26 +0500

    added routes to HistoryController

--- src/main/java/moex/com/totsystems/controller/HistoryController.java
+++ src/main/java/moex/com/totsystems/controller/HistoryController.java
@@ -4,9 +4,12 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moex.com.totsystems.dto.HistoryDto;
+import moex.com.totsystems.dto.HistoryFilterAndPagination;
+import moex.com.totsystems.dto.SimpleHistoryDto;
 import moex.com.totsystems.entity.History;
 import moex.com.totsystems.service.FileStorageService;
 import moex.com.totsystems.service.HistoryService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,17 +29,31 @@ public class HistoryController {
     private final FileStorageService fileStorageService;
 
     @ApiOperation(value = "Получить историю ценных бумаг по дате")
-    @ResponseStatus(HttpStatus.FOUND)
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "date/{date}")
     public List<History> listByDate(@PathVariable String date) {
         return historyService.getAllByDate(LocalDate.parse(date));
     }
 
     @ApiOperation(value = "Получить историю ценной бумаги по secid")
-    @ResponseStatus(HttpStatus.FOUND)
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "secid/{secid}")
     public List<History> listBySecid(@PathVariable String secid) {
         return historyService.getAllBySecid(secid);
+    }
+
+    @ApiOperation(value = "Получить историю..")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "custom-without-paging")
+    public List<SimpleHistoryDto> listCustom() {
+        return historyService.findAllCustom();
+    }
+
+    @ApiOperation(value = "Получить историю с пагинацией, фильтрацией и сортировкой")
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping(value = "custom-with-filter-and-sort")
+    public Page<SimpleHistoryDto> listCustomWithPagination(@RequestBody HistoryFilterAndPagination historyFilterAndPagination) {
+        return historyService.findAllCustomAnother(historyFilterAndPagination);
     }
 
     @ApiOperation(value = "Создать историю")
