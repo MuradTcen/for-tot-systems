4a2ca77d3dba2b5aab8935c555bb1fda23d3d6de
MuradTcen <muradtcen@.gmail.com>
2020-06-10 00:13:38 +0500

    added methods to HistoryRepository

--- src/main/java/moex/com/totsystems/repository/HistoryRepository.java
+++ src/main/java/moex/com/totsystems/repository/HistoryRepository.java
@@ -1,7 +1,10 @@
 package moex.com.totsystems.repository;
 
+import moex.com.totsystems.dto.SimpleHistoryDto;
 import moex.com.totsystems.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface HistoryRepository extends JpaRepository<History, String> {
+public interface HistoryRepository extends JpaRepository<History, String>, JpaSpecificationExecutor<History> {
     List<History> findAllByTradedateEquals(LocalDate localDate);
 
     List<History> findAllBySecid(String secid);
@@ -17,4 +20,7 @@ public interface HistoryRepository extends JpaRepository<History, String> {
     Optional<History> findByTradedateAndSecid(LocalDate date, String secid);
 
     void deleteByTradedateAndSecid(LocalDate date, String secid);
+
+    @Query("select new moex.com.totsystems.dto.SimpleHistoryDto(h) from moex.com.totsystems.entity.History h")
+    List<SimpleHistoryDto> findAllCustom();
 }
