c3e3152a130c321e612373d5c461b22843485bab
MuradTcen <muradtcen@.gmail.com>
2020-06-15 15:32:29 +0500

    [try for deploy] changed app.js

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -39,6 +39,7 @@ function App() {
                     </Col>
                 </Row>
                 {/*<Footer/>*/}
+                {/*<Footer/>*/}
             </Container>
         </Router>
     );
